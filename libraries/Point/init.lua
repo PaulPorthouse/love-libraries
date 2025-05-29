12878c43be2b9d44ef635e1f24aa88897748d22d
Paul Porthouse <paul@porthouse.me.uk>
2025-05-29 16:36:51 +0100

    Removed the z property of the point and added a constrain function

--- libraries/Point/init.lua
+++ libraries/Point/init.lua
@@ -2,28 +2,15 @@ local Point = {}
 Point.__index = Point
 Point.__version = "0.0.1"
 
-function Point:new(x, y, z)
-    local p = { x = x or 0, y = y or 0, z = z or 0 }
+function Point:new(x, y)
+    local p = { x = x or 0, y = y or 0 }
     setmetatable(p, self)
     return p
 end
 
-function Point:move(dx, dy, dz)
+function Point:move(dx, dy)
     self.x = self.x + (dx or 0)
     self.y = self.y + (dy or 0)
-    self.z = self.z + (dz or 0)
-end
-
-function Point:clamp(minX, minY, maxX, maxY, minZ, maxZ)
-    minX = minX or 0; maxX = maxX or 0
-    minY = minY or 0; maxY = maxY or 0
-    minZ = minZ or 0; maxZ = maxZ or 0
-    if self.x < minX then self.x = minX end
-    if self.y < minY then self.y = minY end
-    if self.z < minZ then self.z = minZ end
-    if self.x > maxX then self.x = maxX end
-    if self.y > maxY then self.y = maxY end
-    if self.z > maxZ then self.z = maxZ end
 end
 
 function Point:angle(other)
@@ -37,6 +24,12 @@ function Point:distance(other)
     local dx = other.x - self.x
     local dy = other.y - self.y
     return math.sqrt(dx * dx + dy * dy)
+end
+
+function Point:constrain(rectangle)
+    local bb = rectangle:toBoundingBox()
+    self.x = math.clamp(bb.x1, bb.x2, self.x)
+    self.y = math.clamp(bb.y1, bb.y2, self.y)
 end
 
 return Point
--- libraries/Point/tests.lua
+++ libraries/Point/tests.lua
@@ -1,6 +1,7 @@
 local Tests = {}
 
 local Point = LoveLibraries:loadLibrary("point")
+local Rectangle = LoveLibraries:loadLibrary("rectangle")
 
 function Tests:start()
     local fs = table.functions(self, "^test__")
@@ -20,25 +21,27 @@ function Tests.test__constructor()
     assert(p1.y == 2, "X value is incorrect. Expected 2, set to " .. p1.y)
 end
 
-function Tests.test__clamp()
-    -- test clamp minX
+function Tests.test__constrain()
+    local rect = Rectangle:new(0, 0, 500, 500)
+
+    -- test constrain minX
     local p2 = Point:new(-10, 100)
-    p2:clamp(0, 0, 500, 500)
+    p2:constrain(rect)
     assert(p2.x == 0, "X has not been clamped. Expected 0, set to " .. p2.x)
 
-    -- test clamp minY
+    -- test constrain minY
     local p3 = Point:new(100, -10)
-    p3:clamp(0, 0, 500, 500)
+    p3:constrain(rect)
     assert(p3.y == 0, "Y has not been clamped. Expected 0, set to " .. p3.y)
 
-    -- test clamp minX
+    -- test constrain minX
     local p4 = Point:new(600, 100)
-    p4:clamp(0, 0, 500, 500)
+    p4:constrain(rect)
     assert(p4.x == 500, "X has not been clamped. Expected 500, set to " .. p4.x)
 
-    -- test clamp minY
+    -- test constrain minY
     local p5 = Point:new(100, 600)
-    p5:clamp(0, 0, 500, 500)
+    p5:constrain(rect)
     assert(p5.y == 500, "Y has not been clamped. Expected 500, set to " .. p5.y)
 end
 
